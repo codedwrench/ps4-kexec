ee391189fad9b7b05aadcee5f22298c4942ebbbb
Sergey Lisov <sleirsgoevy@gmail.com>
2021-01-12 21:31:56 +0300

    Fix memory map not reflecting the vram_gb setting

--- kexec.c
+++ kexec.c
@@ -148,9 +148,9 @@ int sys_kexec(void *td, struct sys_kexec_args *uap)
     // Initialize bp
     // TODO should probably do this from cpu_quiesce_gate, then bp doesn't
     // need to be allocated here, just placed directly into low mem
-    prepare_boot_params(bp, image);
-
     set_nix_info(image, bp, initramfs, initramfs_size, cmd_line, uap->vram_gb);
+
+    prepare_boot_params(bp, image);
 
     // Hook the final ICC shutdown function
     if (!kernel_hook_install(hook_icc_query_nowait, icc_query_nowait)) {
